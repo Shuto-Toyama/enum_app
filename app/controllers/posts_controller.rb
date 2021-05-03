3d3ef92576036e318b8ee430991708e8d38d25fe
Stoyama <Stoyama@MacBook.local>
2021-05-04 06:51:15 +0900

    add_posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,12 @@ class PostsController < ApplicationController
 
   def create
     Post.create!(post_params)
-    redirect_to post_path
+    redirect_to posts_path
   end
 
-  private 
+  private
 
   def post_params
-    paramas,require(:post).permit(:genre, :content)
+    params.require(:post).permit(:genre, :content)
   end
 end
