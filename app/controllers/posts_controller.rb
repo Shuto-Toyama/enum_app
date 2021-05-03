ffa424ff26df68f22cbaf31f47e3026b50847864
Stoyama <Stoyama@MacBook.local>
2021-05-04 06:12:36 +0900

    add_view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(:id)
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    Post.create!(post_params)
+    redirect_to post_path
+  end
+
+  private 
+
+  def post_params
+    paramas,require(:post).permit(:genre, :content)
   end
 end
